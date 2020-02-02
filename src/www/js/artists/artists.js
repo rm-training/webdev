c1ba043d9b93c786b7300f42d3835355686ac571
Ryan Morris <mr.morris@gmail.com>
2020-02-02 13:43:25 -0500

    Updating the artists exercise

--- src/www/js/artists/artists.js
+++ src/www/js/artists/artists.js
@@ -10,14 +10,24 @@
 // ID of "artists").  Display the name of each artist inside the newly
 // created <li> elements.
 //
+//
 // BONUS #1:
+//
+// We want to display a loading indicator when the user clicks the button.
+// The SemanticUI Style Library has a class that displays a loading "spinner".
+//
+// Make it so that when the button is clicked, a class of "loading" is added
+// to the button element's class list
+// And, after the content is loaded, the "loading" class is removed.
+//
+// BONUS #2:
 //
 // Clicking one of the <li> elements should display all information
 // about the clicked artist in the <ul> with the ID of "details".
 // (Hint: make another HTTP request to /api/artists/N where N is the
 // artist ID.)
 //
-// BONUS #2:
+// BONUS #3:
 //
 // After displaying a list of artist details, also display a list of
 // album names.  A list of albums can be fetched using the following
@@ -25,4 +35,4 @@
 //     /api/artists/N/albums
 //
 
-// Your code here.
+// your solution here
