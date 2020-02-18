2e2cad8d98c0ca078549951b8d6df8a6ef1d05ad
Ryan Morris <mr.morris@gmail.com>
2020-02-18 08:11:48 -0500

    Expand on lab

--- src/www/js/primitives/primitives.js
+++ src/www/js/primitives/primitives.js
@@ -9,17 +9,36 @@ function exercise1() {
   return 1 + 2;
 }
 
-/******************************************************************************/
-// EXERCISE 2: Create (and return) a string with at least 5 characters
+/**
+ * EXERCISE 2: Create (and return) a string with at least 5 characters
+ */
 function exercise2() {
 
   // Your code here.
 
 }
 
-/******************************************************************************/
-// EXERCISE 3: Create (and return) an array that has at least 3 elements
+/**
+ * EXERCISE 3: Create (and return) an array that has at least 3 elements
+ */
 function exercise3() {
+
+  
+}
+
+/**
+ * EXERCISE 4: return the value of the string with "**" on either side
+ */
+function exercise4(str) {
+
+}
+
+/**
+ * EXERCISE 5: create a return a function that adds 10 to the given `value` argument and returns it
+ *
+ * BONUS: Handle when no value is given, ie: exercise5();
+ */
+function exercise5(value) {
 
   
 }
