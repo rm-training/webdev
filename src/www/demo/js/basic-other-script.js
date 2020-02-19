b1f09eaab0e02cffb7f717230236ce6c8e4c5fba
Ryan Morris <mr.morris@gmail.com>
2020-02-19 10:07:57 -0800

    Solved the basic stuff

--- src/www/demo/js/basic-other-script.js
+++ src/www/demo/js/basic-other-script.js
@@ -1,45 +1,58 @@
+'use strict';
+
 console.log('My other script');
 
-// is this safe?
-let user;
+const ApplicationModule = (function initApplication () {
+  let user;
 
-const checkNameLength = function() {
-  // anything wrong here?
-  length = name.length;
-  // anything wrong here?
-  reversed = name.split("").reverse().join("");
+  const checkNameLength = function(name) {
+    const length = name.length;
+    const reversed = name
+      .split("")
+      .reverse()
+      .join("");
 
-  console.log('Name is ' + length + ' long');
-  console.log(`Reversed, it is ${reversed}`);
-}
+    console.log('Name is ' + length + ' long');
+    console.log(`Reversed, it is ${reversed}`);
+  }
 
-const authenticateUser = function () {
-  username = prompt("What is your username?");
-  user = getUser(username);
-}
+  const authenticateUser = function () {
+    const username = prompt("What is your username?");
+    user = getUser(username);
+  }
 
-// nothing wrong here...
-// pretend this makes a request to some API/endpoint...
-// and returns the user
-const getUser = function (username) {
+  // nothing wrong here...
+  // pretend this makes a request to some API/endpoint...
+  // and returns the user
+  const getUser = function (username) {
+    return {
+      id: 5,
+      username: username,
+      email: 'some@email.com',
+      records: ['a', 'b', 'c']
+    };
+  }
+
+  function checkRecords(records) {
+    let lastRecord = records[records.length - 1]; // pop() takes the last element off an array
+
+    console.log(`Your last record is ${lastRecord}`);
+  }
+
+  authenticateUser();
+  checkNameLength(user.username);
+  checkRecords(user.records);
+
+  console.log('At the very end user is...', user);
+
   return {
-    id: 5,
-    username: username,
-    email: 'some@email.com',
-    records: ['a', 'b', 'c']
-  };
-}
+    check: checkRecords,
+    seeUser: function () {
+      console.log(user);
+    }
+  }
+})();
 
-// anything wrong here?
-function checkRecords(records) {
-  let lastRecord = records.pop();
-
-  console.log(`Your last record is ${lastRecord}`);
-}
-
-authenticateUser();
-checkNameLength(user.username);
-checkRecords(user.records);
 
 // what are the user's records after the above?
 //console.log('User records after all the work is done...', user.records);
--- src/www/demo/js/basic.js
+++ src/www/demo/js/basic.js
@@ -1,6 +1,12 @@
+import checkNameLength from './basic-other-script.js';
+
 console.log('The beginning of the basic app');
 
 alert('Welcome!');
+
 const user = prompt('What is your name?');
 alert(`Welcome ${user}`);
 
+console.log('I am done running');
+
+ApplicationModule.check();
